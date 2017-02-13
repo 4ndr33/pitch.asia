b35e6b2d7c72f83ce47f08c98f2ed66afad722e4
4ndr33 <dede1488@gmail.com>
2017-02-13 09:53:36 +0700

    f

--- functions.php
+++ functions.php
@@ -1,88 +1,235 @@
 <?php
 
-function wptuts_styles_with_the_lot()
-{
-    // Register the style like this for a theme:
-    wp_register_style( 'custom-style', get_template_directory_uri() . '/vendor/bootstrap/css/bootstrap.min.css', array(), '20120208', 'all' );
-    // For either a plugin or a theme, you can then enqueue the style:
-    wp_enqueue_style( 'custom-style' );
-	wp_register_style( 'custom-style2', get_template_directory_uri() . '/vendor/metisMenu/metisMenu.min.css', array(), '20120208', 'all' );
-    // For either a plugin or a theme, you can then enqueue the style:
-    wp_enqueue_style( 'custom-style2' );
-	
-	wp_register_style( 'custom-style3', get_template_directory_uri() . '/dist/css/sb-admin-2.css', array(), '20120208', 'all' );
-    // For either a plugin or a theme, you can then enqueue the style:
-    wp_enqueue_style( 'custom-style3' );
-	
-	wp_register_style( 'custom-style4', get_template_directory_uri() . '/vendor/morrisjs/morris.css', array(), '20120208', 'all' );
-    // For either a plugin or a theme, you can then enqueue the style:
-    wp_enqueue_style( 'custom-style4' );
-	
-	wp_register_style( 'custom-style5', get_template_directory_uri() . '/vendor/font-awesome/css/font-awesome.min.css', array(), '20120208', 'all' );
-    // For either a plugin or a theme, you can then enqueue the style:
-    wp_enqueue_style( 'custom-style5' );
-	
-	 // Register the script like this for a theme:
-    wp_register_script( 'custom-script', get_template_directory_uri() . '/vendor/jquery/jquery.min.js', array(), '20120208',true );
-    // For either a plugin or a theme, you can then enqueue the script:
-    wp_enqueue_script( 'custom-script');
-	
-	// Register the script like this for a theme:
-    wp_register_script( 'custom-script2', get_template_directory_uri() . '/vendor/bootstrap/js/bootstrap.min.js', array(), '20120208',true );
-    // For either a plugin or a theme, you can then enqueue the script:
-    wp_enqueue_script( 'custom-script2');
-	
-	// Register the script like this for a theme:
-    wp_register_script( 'custom-script3', get_template_directory_uri() . '/vendor/metisMenu/metisMenu.min.js', array(), '20120208',true );
-    // For either a plugin or a theme, you can then enqueue the script:
-    wp_enqueue_script( 'custom-script3');
-	
-	// Register the script like this for a theme:
-    wp_register_script( 'custom-script4', get_template_directory_uri() . '/vendor/raphael/raphael.min.js', array(), '20120208',true );
-    // For either a plugin or a theme, you can then enqueue the script:
-    wp_enqueue_script( 'custom-script4');
-	
-	// Register the script like this for a theme:
-    wp_register_script( 'custom-script5', get_template_directory_uri() . '/vendor/morrisjs/morris.min.js', array(), '20120208',true );
-    // For either a plugin or a theme, you can then enqueue the script:
-    wp_enqueue_script( 'custom-script5');
-	
-	// Register the script like this for a theme:
-    wp_register_script( 'custom-script6', get_template_directory_uri() . '/data/morris-data.js', array(), '20120208',true );
-    // For either a plugin or a theme, you can then enqueue the script:
-    wp_enqueue_script( 'custom-script6');
-	
-	
-	// Register the script like this for a theme:
-    wp_register_script( 'custom-script7', get_template_directory_uri() . '/dist/js/sb-admin-2.js', array(), '20120208',true );
-    // For either a plugin or a theme, you can then enqueue the script:
-    wp_enqueue_script( 'custom-script7');
-	
-	
+
+//This function below will NOT reset the user's password when they are confirmed by the "New User Approve" plugin. If we remove this function, then the user will always get a reset new password when they are approved, and we do not want that to happen
+
+function ignore_new_user_autopass() {
+	return true;
 }
-add_action( 'wp_enqueue_scripts', 'wptuts_styles_with_the_lot' );
 
-/*
-function wptuts_styles_with_the_lot2()
-{
-    // Register the style like this for a theme:
-    wp_register_style( 'custom-style2', get_template_directory_uri() . '/vendor/metisMenu/metisMenu.min.css', array(), '20120208', 'all' );
- 
-    // For either a plugin or a theme, you can then enqueue the style:
-    wp_enqueue_style( 'custom-style2' );
+add_filter( 'new_user_approve_bypass_password_reset', 'ignore_new_user_autopass' );
+
+
+
+//This changes the bbpress plugin role names given to users
+function my_custom_roles( $role, $user_id, $user_role ) {
+	if( $role == 'Keymaster' )
+		return 'Board Admin';
+
+	return $role;
 }
-add_action( 'wp_enqueue_scripts', 'wptuts_styles_with_the_lot2' );
+
+add_filter( 'bbp_get_user_display_role', 'my_custom_roles', 10, 3 );
 
 
 
-function wptuts_scripts_basic()
-{
-    // Register the script like this for a theme:
-    wp_register_script( 'custom-script', get_template_directory_uri() . '/vendor/bootstrap/css/bootstrap.min.css' );
- 
-    // For either a plugin or a theme, you can then enqueue the script:
-    wp_enqueue_script( 'custom-script' );
+function listing_published_send_email($post_id) {
+   $post = get_post($post_id);
+   $author = get_userdata($post->post_author);
+   $message = "
+      Hi ".$author->display_name.",
+      Your Pitch.Asia job listing, ".$post->post_title." has just been approved at ".get_permalink( $post_id )."
+
+You can visit https://www.Pitch.Asia and login to manage your job listing.
+
+Many thanks,
+Pitch.Asia
+https://www.Pitch.Asia
+   ";
+   wp_mail($author->user_email, "Your job listing is online", $message);
 }
-add_action( 'wp_enqueue_scripts', 'wptuts_scripts_basic' );
-*/
-?>
+add_action('publish_job_listing', 'listing_published_send_email');
+
+
+
+
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+  register_post_type( 'media_query',
+    array(
+      'labels' => array(
+        'name' => __( 'Media Queries' ),
+        'singular_name' => __( 'Media Query' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'media-query'),
+        'show_ui'               => true,
+		'supports'              => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail' ),
+    )
+  );
+  register_post_type( 'pitchasianews',
+    array(
+      'labels' => array(
+        'name' => __( 'PitchAsia News Posts' ),
+        'singular_name' => __( 'PitchAsia News Post' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'pitchasianews'),
+        'show_ui'               => true,
+		'supports'              => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail' ),
+    )
+  );
+}
+
+
+function my_taxonomies_media_query() {
+  $labels = array(
+    'name'              => _x( 'Media Query Categories', 'taxonomy general name' ),
+    'singular_name'     => _x( 'Media Query Category', 'taxonomy singular name' ),
+    'search_items'      => __( 'Search Media Query Categories' ),
+    'all_items'         => __( 'All Media Query Categories' ),
+    'parent_item'       => __( 'Parent Media Query Category' ),
+    'parent_item_colon' => __( 'Parent Media Query Category:' ),
+    'edit_item'         => __( 'Edit Media Query Category' ), 
+    'update_item'       => __( 'Update Media Query Category' ),
+    'add_new_item'      => __( 'Add New Media Query Category' ),
+    'new_item_name'     => __( 'New Media Query Category' ),
+    'menu_name'         => __( 'Media Query Categories' ),
+  );
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => true,
+  );
+  register_taxonomy( 'media_query_category', 'media_query', $args );
+}
+add_action( 'init', 'my_taxonomies_media_query', 0 );
+
+
+
+
+
+
+function my_taxonomies_pitchasianews() {
+  $labels = array(
+    'name'              => _x( 'PitchAsia News Categories', 'taxonomy general name' ),
+    'singular_name'     => _x( 'PitchAsia News Category', 'taxonomy singular name' ),
+    'search_items'      => __( 'Search PitchAsia News Categories' ),
+    'all_items'         => __( 'All PitchAsia News Categories' ),
+    'parent_item'       => __( 'Parent PitchAsia News Category' ),
+    'parent_item_colon' => __( 'Parent PitchAsia News Category:' ),
+    'edit_item'         => __( 'Edit PitchAsia News Category' ), 
+    'update_item'       => __( 'Update PitchAsia News Category' ),
+    'add_new_item'      => __( 'Add New PitchAsia News Category' ),
+    'new_item_name'     => __( 'New PitchAsia News Category' ),
+    'menu_name'         => __( 'PitchAsia News Categories' ),
+  );
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => true,
+  );
+  register_taxonomy( 'pitchasianews_category', 'pitchasianews', $args );
+}
+add_action( 'init', 'my_taxonomies_pitchasianews', 0 );
+
+
+
+
+//This adds a bubble showing the number of pending posts in a category within the admin area
+//If we want to add a new custom post type, we just add it to the array in the same way we just added "media_query"
+function show_pending_number($menu) {    
+    $types = array("post", "page", "custom-post-type", "media_query", "job_listing");
+    $status = "pending";
+    foreach($types as $type) {
+        $num_posts = wp_count_posts($type, 'readable');
+        $pending_count = 0;
+        if (!empty($num_posts->$status)) $pending_count = $num_posts->$status;
+
+        if ($type == 'post') {
+            $menu_str = 'edit.php';
+        } else {
+            $menu_str = 'edit.php?post_type=' . $type;
+        }
+
+        foreach( $menu as $menu_key => $menu_data ) {
+            if( $menu_str != $menu_data[2] )
+                continue;
+            $menu[$menu_key][0] .= " <span class='update-plugins count-$pending_count'><span class='plugin-count'>" . number_format_i18n($pending_count) . '</span></span>';
+            }
+        }
+    return $menu;
+}
+add_filter('add_menu_classes', 'show_pending_number');
+
+
+
+
+
+/***********************EDITED BY RAVI 9-DEC-2015**************************/
+function emailAlias($email,$frdemail,$action) {
+	$whmusername = "pitchasia";
+	$whmpassword = "NEWMnUUj74RfgWeLD2EzDEo4S8ScNEW";
+	
+	if($action == 'add'){
+	$query = "https://192.111.132.84:2083/cpsess1973261420/execute/Email/add_forwarder?domain=mediaquery.pitchasia.com&email=$email&fwdopt=fwd&fwdemail=$frdemail";
+	} elseif ($action == 'delete') {
+	$query = "https://192.111.132.84:2083/cpsess1973261420/execute/Email/delete_forwarder?address=$email&forwarder=$frdemail";
+	}
+	 
+	$curl = curl_init();                                // Create Curl Object
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER,0);       // Allow self-signed certs
+	curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);       // Allow certs that do not match the hostname
+	curl_setopt($curl, CURLOPT_HEADER,0);               // Do not include header in output
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);       // Return contents of transfer on curl_exec
+	$header[0] = "Authorization: Basic " . base64_encode($whmusername.":".$whmpassword) . "\n\r";
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $header);    // set the username and password
+	curl_setopt($curl, CURLOPT_URL, $query);            // execute the query
+	$result = curl_exec($curl);
+	if ($result == false) {
+	    error_log("curl_exec threw error \"" . curl_error($curl) . "\" for $query");   
+	                                                    // log error if curl exec fails
+	}
+	curl_close($curl);
+}
+
+//publish a post
+add_action('save_post','save_post_callback');
+function save_post_callback($post_id){
+
+	global $post;
+    if (isset($post) && $post->post_type != 'media_query'){
+        return;
+    }	
+	$status = get_post_status( $post_id );
+	
+	$alias = $post_id .date("y").date("m").rand(100,9999).'@mediaquery.pitchasia.com';
+	
+    if ( ! get_post_meta( $post_id, 'mail_alias', $alias, false ) ) {
+		if ( ! add_post_meta( $post_id, 'mail_alias', $alias, true )  )	{	
+           update_post_meta ( $post_id, 'mail_alias', $alias );
+		}
+	}		
+
+	$email =  get_post_meta( $post_id, 'contact_email', false );
+	$t_email =  get_post_meta( $post_id, 'mail_alias', false );
+	
+	$name = explode('@',$t_email[0]);
+
+	if($status == 'publish'){
+		emailAlias($name[0],$email[0],'add');
+	
+	} else {		
+		emailAlias($t_email[0],$email[0],'delete');
+	}
+
+}
+
+
+//Delete a post
+add_action('delete_post','delete_post_callback');
+
+function delete_post_callback(){
+		
+	global $post;
+	
+	(isset($_GET['pid']))? $post_id = $_GET['pid'] : $post_id = $post->ID;
+	
+	if (get_post_type($post_id) != 'media_query'){
+        return;
+    }
+
+	$email =  get_post_meta( $post_id, 'contact_email', false );
+	$t_email =  get_post_meta( $post_id, 'mail_alias', false );
+	emailAlias($t_email[0],$email[0],'delete');				
+}
---
+++ header.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * Header Section of Profound Theme.
+ * 
+ * @package Profound
+ */
+?>
+<!DOCTYPE html >
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes() ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes() ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes() ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes() ?>>
+<!--<![endif]-->
+
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php wp_head(); ?>
+<?php
+//Added to aid the WPUF Plugin accurately delete the mail_alias from virtual user table if
+//a user manually deletes a media_query from their own frontend. This is necessary because
+//WPUF plugin doesn't use the default Wordpress delete function. We also added something to
+//functions.php for this delete to work.
+if(isset($_GET['action']) && $_GET['action'] == 'del'){
+	delete_post_callback();	
+}
+?>	
+</head>
+
+<body <?php body_class(); ?>>
+    <div id="parent-wrapper" class="parent-wrapper grid-col-16">
+        <div id="wrapper" class="wrapper grid-col-16">
+            
+            <?php if(!profound_get_option('disable_header')): ?>
+            <div class="header-bg-section clearfix">
+                <div id="header-section" class="header-section grid-col-16 clearfix">
+                    <div id="logo-section" class="logo-section grid-col-7 grid-float-left clearfix"><?php profound_logo() ?></div>
+
+
+
+<?php
+if ( is_user_logged_in() ) {
+	echo '<div align=right>';
+	echo '<a href="';
+echo wp_logout_url( get_bloginfo('url') );
+	echo '" class="button-red">&nbsp;&nbsp; LOGOUT &nbsp;&nbsp;</a><br><br>';
+	echo '</div>';
+
+
+} else {
+
+
+
+	echo '<div align=right>';
+	echo '<a href="https://www.pitch.asia/login/" class="button-pink">&nbsp;&nbsp; LOGIN &nbsp;&nbsp;</a><br><br>';
+	echo '<a href="https://www.pitch.asia/register-expertsource/" class="button-green">&nbsp;&nbsp; Expert Source Registration &nbsp;&nbsp;</a>';
+	echo '&nbsp;&nbsp;&nbsp;';
+	echo '<a href="https://www.pitch.asia/register-journalist/" class="button-blue">&nbsp;&nbsp; Journalist Registration &nbsp;&nbsp;</a>';
+	echo '<br /><br /><strong>Join 20,000+ Journalists and 85,000+ Experts Building Great Stories!</strong>';
+}
+?>
+
+</div>
+
+
+                    <?php // profound_social_section_show() ?>
+                </div><!-- header section ends -->
+                
+            </div><!-- header bg section ends -->
+            <?php if (!profound_get_option('disable_menu')): ?>
+            <div id="nav-section" class="nav-section grid-col-16 grid-float-left clearfix">
+                <div id="primarymenu-resp" class="primarymenu-resp"><span><?php _e('Menu', 'profound') ?></span><i class="mdf mdf-bars"></i></div>
+                <div id="primarymenu-section" class="primarymenu-section nav"><?php profound_nav() ?></div>
+            </div>
+            <?php endif; ?>
+
+            <?php endif; ?>
+                        
+            <div id="main-section" class="main-section grid-col-16 clearfix">
